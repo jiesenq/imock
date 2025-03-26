6ddbfbed4ad6345706ed1e1f8044efdb6edf91ca
qinyikl <qinyikl@noreply.gitcode.com>
2025-03-27 00:50:19 +0800

    优化

--- src/MockTreeDataProvider.ts
+++ src/MockTreeDataProvider.ts
@@ -1,9 +1,12 @@
-// src/MockTreeDataProvider.ts
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export class MockTreeDataProvider implements vscode.TreeDataProvider<MockItem> {
-  private _onDidChangeTreeData: vscode.EventEmitter<MockItem | undefined | null | void> = new vscode.EventEmitter<MockItem | undefined | null | void>();
-  readonly onDidChangeTreeData: vscode.Event<MockItem | undefined | null | void> = this._onDidChangeTreeData.event;
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    MockItem | undefined | null | void
+  > = new vscode.EventEmitter<MockItem | undefined | null | void>();
+  readonly onDidChangeTreeData: vscode.Event<
+    MockItem | undefined | null | void
+  > = this._onDidChangeTreeData.event;
 
   getTreeItem(element: MockItem): vscode.TreeItem {
     return element;
@@ -11,17 +14,16 @@ export class MockTreeDataProvider implements vscode.TreeDataProvider<MockItem> {
 
   getChildren(element?: MockItem): Thenable<MockItem[]> {
     if (!element) {
-      // 根节点数据
+      // 返回根节点的子节点
       return Promise.resolve([
-        new MockItem('Mock 服务 1', vscode.TreeItemCollapsibleState.Collapsed),
-        new MockItem('Mock 服务 2', vscode.TreeItemCollapsibleState.Collapsed)
+        new MockItem("Mock Data 1", vscode.TreeItemCollapsibleState.Collapsed),
+        new MockItem("Mock Data 2", vscode.TreeItemCollapsibleState.Collapsed),
       ]);
     }
-
-    // 子节点数据
+    // 返回子节点的子节点（如果有）
     return Promise.resolve([
-      new MockItem('子项 1', vscode.TreeItemCollapsibleState.None),
-      new MockItem('子项 2', vscode.TreeItemCollapsibleState.None)
+      new MockItem("Sub Mock Data 1", vscode.TreeItemCollapsibleState.None),
+      new MockItem("Sub Mock Data 2", vscode.TreeItemCollapsibleState.None),
     ]);
   }
 
