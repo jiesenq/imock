4d28112c7781e3581d9ea82fc11c098a327825fd
Jason.Qin <jason.qin@aliyun.com>
2025-04-03 05:17:18 +0800

    add menus page

--- src/views/browser.ts
+++ src/views/browser.ts
@@ -1,14 +1,18 @@
 import Data from "../data/types";
+import * as fs from "fs";
 import { readFileSync } from "fs";
-// import CONST_WEBVIEW from "../data/webview";
+import * as path from "path";
 
 interface WebviewContext {
   extensionPath: string;
   webviewUri: string;
 }
 export const mockViewTemplate = (webviewContext: WebviewContext) => {
+  // 假设原本读取 mock-api.html 的代码如下
+  // const htmlFilePath = path.join(__dirname, 'mock-api.html');
+
   let html = readFileSync(
-    webviewContext.extensionPath + "src/views/pages/mock-api.html",
+    webviewContext.extensionPath + "src/views/pages/menus.html",
     "utf8"
   );
   return html;
